38b2d565cdbf94afee01521d9645b6a12b145234
Bez Worcou <beazaworcou@gmail.com>
2020-08-07 17:25:43 +1000

    first commit

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,27 @@
-<? snippet('header') ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?= $site->title() ?></title>
+  </head>
+  <body>
 
-    <h1><?= $page->title() ?></h1>
+    <header>
+      <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
 
-<? snippet('footer') ?>
+      <nav class="menu">
+        <?php foreach ($site->children() as $subpage): ?>
+          <a href="><?= $subpage->url() ?>"<?= $subpage->title() ?></a>
+        <?php endforeach ?>
+      </nav>
+
+    </header>
+
+    <? snippet('header') ?>
+    Hello World
+        <h1><?= $page->title() ?></h1>
+        <?= $page->text() ?>
+        <?= $page->contact() ?>
+    <? snippet('footer') ?>
+  </body>
+</html>
